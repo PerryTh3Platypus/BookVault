12d8f2bdd603690ce30874a4d846ef29bed20bc2
OV3RDR1VE <forgotten_dead_knight@tutanota.com>
2024-01-03 18:30:13 +0200

    main method update

--- src/main/java/com/github/perryth3platypus/Main.java
+++ src/main/java/com/github/perryth3platypus/Main.java
@@ -1,25 +1,16 @@
 package com.github.perryth3platypus;
 
 import com.formdev.flatlaf.FlatDarkLaf;
-import com.formdev.flatlaf.FlatLightLaf;
-import com.formdev.flatlaf.themes.FlatMacDarkLaf;
 import com.github.perryth3platypus.controller.DatabaseConnector;
-import com.github.perryth3platypus.controller.DatabaseController;
 import com.github.perryth3platypus.gui.DatabaseConnectorFrame;
-import com.github.perryth3platypus.gui.MainFrame;
-import com.github.perryth3platypus.model.Book;
-import com.github.perryth3platypus.model.InternalLocation;
-import com.github.perryth3platypus.model.Series;
 
 import javax.swing.*;
-import java.util.concurrent.CountDownLatch;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         FlatDarkLaf.setup();
 
         DatabaseConnector dbConnector = new DatabaseConnector();
-        DatabaseController dbController = new DatabaseController(dbConnector);
 
         DatabaseConnectorFrame databaseConnectorFrame = new DatabaseConnectorFrame(dbConnector);
         SwingUtilities.invokeLater(() -> databaseConnectorFrame.start());
